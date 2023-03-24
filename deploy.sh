822a07100da07ff2efe0598cf89ad6c84227655b
liangyong9098@cvte.com <liangyong9098@cvte.com>
2023-03-24 17:06:40 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ git add .
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:<loycoder>/<loycoder>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # git push -f origin  master:master
 
